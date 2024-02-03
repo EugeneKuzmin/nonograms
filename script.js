9ef360b6e436724173c66d4baf6f130b6d3ee7c9
Gennarro@yandex.ru <Gennarro@yandex.ru>
2024-02-03 21:22:45 +0300

    refact:draw nonogram lines code function

--- script.js
+++ script.js
@@ -41,6 +41,50 @@ const getScheme = (level) => {
   return {header:headerNonogram,body:bodyNonogram,sideHeader:sideHeaderNonogram};
 }
 
+const drawBorderBottom = (rowIndx,bodyLength,cellElement) => {
+  if((rowIndx + 1) !== bodyLength){
+    if((rowIndx + 1) % 5 ===0){
+      cellElement.classList.add('border-bottom-2');
+    }else{
+      cellElement.classList.add('border-bottom-1');
+    }
+  }
+  return cellElement
+}
+
+const drawBorderRight = (colIndx,bodyLength,cellElement) => {
+  if( (colIndx + 1) !== bodyLength ){
+    if((colIndx + 1) % 5 === 0){
+      cellElement.classList.add('border-right-2');
+    }else{
+      cellElement.classList.add('border-right-1');
+    }
+  }
+  return cellElement
+}
+
+const drawHeaderBorderBottom = (rowIndx,headerLength,cellElement,isEmpty) => {
+  if((rowIndx + 1) === headerLength){
+    cellElement.classList.add('border-bottom-2');
+  }
+  if(isEmpty){
+    cellElement.classList.add('border-bottom-1');
+  }
+  return cellElement
+}
+
+const drawHeaderBorderRight = (colIndx,sideLenght,cellElement,isEmpty) => {
+
+  if((colIndx + 1) === sideLenght){
+    cellElement.classList.add('border-right-2');
+  }
+
+  if(isEmpty){
+    cellElement.classList.add('border-right-1');
+  }
+  return cellElement
+}
+
 const drawNonogram = (scheme) => {
 
   gridLayout.innerHTML = '';
@@ -50,8 +94,14 @@ const drawNonogram = (scheme) => {
   let sideHeaderNonogram = scheme.sideHeader;
   let headerNonogram = scheme.header;
 
+  const bodyLength = bodyNonogram[0].length;
+  const sideLenght = sideHeaderNonogram[0].length;
+  const headerLength = headerNonogram.length;
+
   gridLayout.classList.add('grid');
-  gridLayout.setAttribute('style', `grid-template-columns: repeat(${sideHeaderNonogram[0].length + headerNonogram[0].length},1fr);max-width: ${(sideHeaderNonogram[0].length + headerNonogram[0].length)*2}rem;`);
+  gridLayout.classList.add('border-nonogram-tbl');
+
+  gridLayout.setAttribute('style', `grid-template-columns: repeat(${sideLenght + bodyLength},1fr);max-width: ${(sideLenght + bodyLength)*2}rem;`);
   
   const gridCells = [];
   
@@ -59,47 +109,55 @@ const drawNonogram = (scheme) => {
   const preHeader = Array.from(Array(sideHeaderNonogram[0].length)).fill(0)
   headerNonogram = headerNonogram.map((row) => [[...preHeader],[...row]])
   
-  
-  headerNonogram.forEach(row =>{
-      row[0].forEach(emptyCell=>{
-          const cellElement = document.createElement('div')
-          cellElement.classList.add('empty-cell')
+  headerNonogram.forEach((row,rowIndx) =>{
+      row[0].forEach((emptyCell,colIndx)=>{
+          let cellElement = document.createElement('div')
+          cellElement = drawHeaderBorderRight(colIndx,sideLenght,cellElement,false);
+          cellElement = drawHeaderBorderBottom(rowIndx,headerLength,cellElement,false);
           gridLayout.appendChild(cellElement)
       });
   
-      row[1].forEach(headCell=>{
-          const cellElement = document.createElement('div')
+      row[1].forEach((headCell,colIndx)=>{
+          let cellElement = document.createElement('div')
           cellElement.textContent = headCell
-          cellElement.classList.add('cell')
+          cellElement.classList.add('min-height-2')
+
+          cellElement = drawBorderRight(colIndx,bodyLength,cellElement);
+          cellElement = drawHeaderBorderBottom(rowIndx,headerLength,cellElement,true);
+
           gridLayout.appendChild(cellElement)
       });
   
       }
   )
   
-  
   bodyNonogram.forEach((row,rowIndx) => {
-      row[0].forEach(clueCell=>{
-              const cellElement = document.createElement('div')
-              cellElement.textContent = clueCell
-              cellElement.classList.add('cell')
-              gridLayout.appendChild(cellElement)
-          });
-          row[1].forEach((headCell,colIndex)=>{
-              const cellElement = document.createElement('div')
-              cellElement.classList.add('clue-cell')
-              cellElement.setAttribute('data-position-row',rowIndx)
-              cellElement.setAttribute('data-position-col',colIndex)
-              cellElement.addEventListener('click',(e)=>
-  
-              {
-                      console.log('row',cellElement.getAttribute('data-position-row'))
-                      console.log('cpl',cellElement.getAttribute('data-position-col'))
-                      cellElement.classList.toggle('picked-dark')
-              })
-              gridCells.push(cellElement)
-              gridLayout.appendChild(cellElement)
+      row[0].forEach((clueCell,colIndx)=>{
+          let cellElement = document.createElement('div');
+          cellElement.textContent = clueCell;
+
+          cellElement = drawHeaderBorderRight(colIndx,sideLenght,cellElement,true);
+          cellElement = drawBorderBottom(rowIndx,bodyLength,cellElement);
+
+          gridLayout.appendChild(cellElement)
+      });
+      row[1].forEach((headCell,colIndx)=>{
+          let cellElement = document.createElement('div')
+          cellElement.classList.add('min-height-2')
+
+          cellElement = drawBorderRight(colIndx,bodyLength,cellElement);
+          cellElement = drawBorderBottom(rowIndx,bodyLength,cellElement);
+
+          cellElement.setAttribute('data-position-row',rowIndx);
+          cellElement.setAttribute('data-position-col',colIndx);
+
+          cellElement.addEventListener('click',(e)=>
+          {
+                  cellElement.classList.toggle('picked-dark')
           })
+          gridCells.push(cellElement)
+          gridLayout.appendChild(cellElement)
+      })
       }
   );
 
